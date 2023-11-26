4bb82fa32112e8e4e5e855aa2ef0b8301d7169ea
Abin M K <abinmkabin@gmail.com>
2023-11-26 23:16:57 +0530

    added dashboard page

--- scripts.js
+++ scripts.js
@@ -1,30 +1,53 @@
 function login() {
-    // Perform login validation (you can add your logic here)
-    // For simplicity, let's assume the login is successful
-    document.getElementById('loginPage').classList.add('hidden');
-    document.getElementById('dashboardPage').classList.remove('hidden');
-    // Display default tab content on login
-    switchTab(1);
+    // Demo credentials (replace with your actual authentication logic)
+    const demoEmail = 'admin';
+    const demoPassword = 'admin';
+
+    // Get user input
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Check if credentials match
+    if (email === demoEmail && password === demoPassword) {
+        // Show the dashboard and hide the login form
+        document.getElementById('loginContainer').style.display = 'none';
+        document.getElementById('dashboardContainer').style.display = 'block';
+        return false; // Prevent form submission
+    } else {
+        // Display an error message (you can customize this part)
+        alert('Invalid email or password. Please try again.');
+        return false; // Prevent form submission
+    }
+}
+
+function showRegister() {
+    // Show the registration form and hide the login and dashboard
+    document.getElementById('loginContainer').style.display = 'none';
+    document.getElementById('dashboardContainer').style.display = 'none';
+    document.getElementById('registerContainer').style.display = 'block';
+}
+
+function register() {
+    // Demo registration logic (replace with your actual registration logic)
+    alert('Registration successful! Redirect to login page.');
+    // Redirect to the login page
+    showLogin();
+    return false; // Prevent form submission
+}
+
+function showLogin() {
+    // Show the login form and hide the dashboard and registration form
+    document.getElementById('loginContainer').style.display = 'block';
+    document.getElementById('dashboardContainer').style.display = 'none';
+    document.getElementById('registerContainer').style.display = 'none';
 }
 
 function switchTab(tabNumber) {
-    // Perform tab switching logic here
     // For simplicity, let's just update the content based on the selected tab
-    const tabContent = document.getElementById('tabContent');
-    switch (tabNumber) {
-        case 1:
-            tabContent.innerHTML = `<h2>System Details</h2>
-                                    <p>This is the content of the System Details tab.</p>`;
-            break;
-        case 2:
-            tabContent.innerHTML = `<h2>Control Options</h2>
-                                    <p>This is the content of the Control Options tab.</p>`;
-            break;
-        case 3:
-            tabContent.innerHTML = `<h2>Calibration Data</h2>
-                                    <p>This is the content of the Calibration Data tab.</p>`;
-            break;
-        default:
-            break;
-    }
+    document.getElementById('tabContent').innerHTML = `Content for Tab ${tabNumber}`;
+}
+
+function logout() {
+    // Show the login form and hide the dashboard and registration form
+    showLogin();
 }
